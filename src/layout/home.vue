e97a75b4120eb839741b48705a83f345d2d5bed7
Allencl <Allen__cc@163.com>
2021-04-28 16:49:09 +0800

    11

--- src/layout/home.vue
+++ src/layout/home.vue
@@ -1,8 +1,18 @@
 
 <template>
-    <div>
-        <h1> 首页</h1>
-        <div id="main_echarts" style="width: 600px;height:500px;"></div>
+    <div class="wis-home">
+        <h1> 前言</h1>
+        <p>
+            规范的UI一方面有助于提升开发的效率，另一方面也有助于培养用户的操作习惯，增加系统的友好性。
+        </p>
+        <p>
+            wis-ui的UI规范，在主题、基础组件、表单组件、整体布局、其它等，五个类别上对系统做了一些规范。这些规范，覆盖了系统80%的页面。
+        </p>
+
+        <div style="text-align:center">
+            <div id="main_echarts" style="width: 600px;height:500px;"></div>
+        </div>
+        
     </div>
 </template>
 <script>
@@ -27,93 +37,104 @@
                 var myChart = echarts.init(document.getElementById('main_echarts'));
                 
                 // 使用刚指定的配置项和数据显示图表。
+                let _data={
+                "name": "wis-ul规范",
+                "children": [
+
+                {
+                "name": "主题",
+                "children": [
+                {"name": "Color颜色", "value": 1082},
+                {"name": "字体", "value": 1336},
+                {"name": "Icon图标", "value": 1681}
+                ]
+                },
+                {
+                "name": "基础组件",
+                "children": [
+                {"name": "按钮", "value": 1082},
+                {"name": "Tabs标签页", "value": 1336},
+                ]
+                },
+
+                {
+                "name": "表单组件",
+                "children": [
+                {"name": "Input文本框", "value": 1082},
+                {"name": "Select下下拉框", "value": 1336},
+                {"name": "Radio", "value": 1681},
+                {"name": "Checkbox", "value": 1681},
+                    {"name": "日期选择器", "value": 1681},
+                    {"name": "时间选择器", "value": 1681},
+                    {"name": "文件上传", "value": 1681},
+                ]
+                },
+                
+                {
+                "name": "整体布局",
+                "children": [
+                {"name": "系统整体布局", "value": 1082},
+                {"name": "Table列表页", "value": 1336},
+                    {"name": "表单提交页", "value": 1336},
+                ]
+                },     
+                
+                {
+                "name": "其它",
+                "children": [
+                {"name": "全局提示", "value": 1082},
+                {"name": "文件预览模块", "value": 1336},
+                    {"name": "404页面", "value": 1336},
+                            {"name": "Loading页面", "value": 1336},
+                ]
+                },  
+                ]
+                }
+
+
                 myChart.setOption({
-                    silent: true,
-                    series: [{
-                        radius: ['15%', '80%'],
-                        type: 'sunburst',
-                        sort: null,
-                        emphasis: {
-                            focus: 'ancestor'
-                        },
-                        data: [{
-                            value: 8,
-                            children: [{
-                                value: 4,
-                                children: [{
-                                    value: 2
-                                }, {
-                                    value: 1
-                                }, {
-                                    value: 1
-                                }, {
-                                    value: 0.5
-                                }]
-                            }, {
-                                value: 2
-                            }]
-                        }, {
-                            value: 4,
-                            children: [{
-                                children: [{
-                                    value: 2
-                                }]
-                            }]
-                        }, {
-                            value: 4,
-                            children: [{
-                                children: [{
-                                    value: 2
-                                }]
-                            }]
-                        }, {
-                            value: 3,
-                            children: [{
-                                children: [{
-                                    value: 1
-                                }]
-                            }]
-                        }],
-                        label: {
-                            color: '#000',
-                            textBorderColor: '#fff',
-                            textBorderWidth: 2,
-                            formatter: function (param) {
-                                var depth = param.treePathInfo.length;
-                                if (depth === 2) {
-                                    return '焊接问题';
-                                }
-                                else if (depth === 3) {
-                                    return '设备漏油';
-                                }
-                                else if (depth === 4) {
-                                    return '表面划伤';
-                                }
-                            }
-                        },
-                        levels: [{}, {
-                            itemStyle: {
-                                color: '#CD4949'
-                            },
+                    // tooltip: {
+                    //     trigger: 'item',
+                    //     triggerOn: 'mousemove'
+                    // },
+                    series: [
+                        {
+                            type: 'tree',
+
+                            data: [_data],
+
+                            top: '10%',
+                            left: '27%',
+                            bottom: '20%',
+                            right: '30%',
+
+                            symbolSize: 7,
+
                             label: {
-                                rotate: '焊接问题'
-                            }
-                        }, {
-                            itemStyle: {
-                                color: '#F47251'
+                                position: 'left',
+                                verticalAlign: 'middle',
+                                align: 'right',
+                    
+                                fontSize: 12
                             },
-                            label: {
-                                rotate: '设备漏油'
-                            }
-                        }, {
-                            itemStyle: {
-                                color: '#FFC75F'
+
+                            leaves: {
+                                label: {
+                                    position: 'right',
+                                    verticalAlign: 'middle',
+                                    align: 'left'
+                                }
                             },
-                            label: {
-                                rotate: '表面划伤'
-                            }
-                        }]
-                    }]
+
+                            emphasis: {
+                                focus: 'descendant'
+                            },
+
+                            expandAndCollapse: true,
+                            animationDuration: 650,
+                            animationDurationUpdate: 750
+                        }
+                    ]
                 });
             }
         }     
@@ -121,5 +142,25 @@
 </script>
 
 <style lang="scss">
+    .wis-home{
+        padding: 50px 60px;
 
+        h1{
+            font-size: 36px;
+            text-align: center;
+            margin-bottom: 22px;
+        }
+        p{
+            text-indent: 26px;
+            font-size: 14px;
+            text-align: justify;
+            line-height: 1.7em;
+            font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,\\5FAE\8F6F\96C5\9ED1,Arial,sans-serif;
+            font-size: 14px;
+            line-height: 1.5;
+            color: #515a6e;
+            -webkit-font-smoothing: antialiased;
+            font-weight: 500;            
+        }
+    }
 </style>
